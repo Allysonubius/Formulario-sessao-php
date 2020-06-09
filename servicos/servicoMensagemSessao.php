e72a8877fad29c95bfead653aecfa05341496da8
Allysom Oliveira <allysomoliveira7@gmail.com>
2020-06-09 20:07:54 -0300

    Finalizado video aula sobre como combinar condições com funções e retornos em PHP.

--- servicos/servicoMensagemSessao.php
+++ servicos/servicoMensagemSessao.php
@@ -1,10 +1,20 @@
-<?php
+<?php /** @noinspection ALL */
 /**Created by Visual Studio Code 
  * User Allyson
  * Date 09/06/2020
 */
 session_start();
 //Funções
+function setarMensagemSucess(string $mensagem) : void
+{
+    $_SESSION['MESSAGE-SUCESS'] = $mensagem;
+}
+function obterMensagemSucess(): ?string
+{
+    if(isset($_SESSION['MESSAGE-SUCESS']))
+        return $_SESSION['MESSAGE-SUCESS'];
+    return null;
+}
 function setarMensagemErro(string $mensagem) : void
 {
     $_SESSION['MESSAGE-ERRO'] = $mensagem;
@@ -15,4 +25,14 @@ function obterMensagemErro(): ?string
         return $_SESSION['MESSAGE-ERRO'];
         return null;
 }
+function removerMensagemErro() : void
+{
+    if(isset($_SESSION['MESSAGE-ERRO']))
+        unset( $_SESSION['MESSAGE-ERRO']);
+}
+function removerMensagemSucess() : void
+{
+    if(isset($_SESSION['MESSAGE-SUCESS']))
+        unset($_SESSION['MESSAGE-SUCESS']);
+{
 ?>
--- servicos/servicoValidacao.php
+++ servicos/servicoValidacao.php
@@ -1,5 +1,5 @@
 <?php
-/**Created by Visual Studio Code 
+/**Created by Visual Studio Code
  * User Allyson
  * Date 09/06/2020
 */
@@ -8,23 +8,29 @@
 //var_dump("Idade: ".$idade);
 //Reformulação de formulário
 
-function validaNome(string $nome) : bool 
+function validarNome(string $nome) : bool
 {
-    if(empty($nome)){
-        $_SESSION['MESSAGE-ERRO'] = 'O nome não pode ser vazio, preencha novamente.';
+    if(empty($nome))
+    {
+        setarMensagemErro('O nome não pode ser vazio, preencha novamente.');
         return false;
-    }else if(strlen($nome) < 3){
-        $_SESSION['MESSAGE-ERRO']  = 'Seu nome têm que ter mais que 3 caracteres ! Preencha novamente seu nome.';
+    }
+    else if(strlen($nome) < 3)
+    {
+        setarMensagemErro( 'Seu nome têm que ter mais que 3 caracteres ! Preencha novamente seu nome.');
         return false;   
-    }else if(strlen($nome) > 30){
-        $_SESSION['MESSAGE-ERRO'] =  'Você ultrapassou 40 caracteres ! Preencha novamente seu nome .';
+    }
+    else if(strlen($nome) > 30)
+    {
+        setarMensagemErro('Você ultrapassou 40 caracteres ! Preencha novamente seu nome .');
         return false;
-    }return true;
+    }
+    return true;
 }
 function validaIdade(string $idade) : bool
 {
     if (!is_numeric($idade)) {
-        $_SESSION['MESSAGE-ERRO'] = 'Informe novamente sua idade.';
+        setarMensagemErro('Informe novamente sua idade.');
         return false;
     }
     return true;
